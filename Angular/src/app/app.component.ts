7db437e5d2d43710f327b8d045c0c8592699446a
AlisherAmonulloev <AlisherAmonulloev@users.noreply.github.com>
2026-06-10 20:45:57 +0300

    Implement Angular example

--- Angular/src/app/app.component.ts
+++ Angular/src/app/app.component.ts
@@ -1,21 +1,66 @@
-import { Component } from '@angular/core';
-import { DxButtonModule, DxButtonTypes } from 'devextreme-angular/ui/button';
+import { Component, ViewChild } from '@angular/core';
+import { DxButtonModule } from 'devextreme-angular/ui/button';
+import { DxTreeListComponent, DxTreeListModule } from 'devextreme-angular/ui/tree-list';
+import dxTreeList from 'devextreme/ui/tree_list';
+import { Node } from 'devextreme/ui/tree_list';
+import { Employee, employees } from './data';
+
+type EmployeeNode = Node<Employee, number>;
 
 @Component({
     selector: 'app-root',
-    imports: [DxButtonModule],
+    imports: [DxTreeListModule, DxButtonModule],
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'Angular';
+  @ViewChild(DxTreeListComponent, { static: false })
+  treeList!: DxTreeListComponent;
 
-  counter = 0;
+  employees = employees;
 
-  buttonText = 'Click count: 0';
+  expandedRowKeys = [1, 2, 10];
 
-  onClick(_e: DxButtonTypes.ClickEvent): void {
-    this.counter++;
-    this.buttonText = `Click count: ${this.counter}`;
+  selectedText = '';
+
+  onButtonClick(): void {
+    const treeList = this.treeList.instance as dxTreeList<Employee, number>;
+    const selectedRowKeys = treeList.getSelectedRowKeys();
+    const names = this.getAllSelectedNodes(treeList, selectedRowKeys)
+      .map((node) => node.data?.Full_Name)
+      .join(', ');
+
+    this.selectedText = `Selected row keys: ${names}`;
+  }
+
+  private getAllSelectedNodes(
+    treeListInstance: dxTreeList<Employee, number>,
+    parentKeys: number[],
+    skipParent?: boolean,
+  ): EmployeeNode[] {
+    let result: EmployeeNode[] = [];
+
+    parentKeys.forEach((key) => {
+      const insertIndex = result.length;
+      const node = treeListInstance.getNodeByKey(key);
+      let parentNode: EmployeeNode | undefined = node.parent;
+      const childKeys = (node.children ?? []).map((child) => child.key);
+
+      while (parentNode && parentNode.level >= 0 && !skipParent) {
+        const currentParent = parentNode;
+        const isCollected = result.some((nodeItem) => nodeItem.key === currentParent.key);
+        if (!isCollected && treeListInstance.isRowSelected(currentParent.key)) {
+          result.splice(insertIndex, 0, currentParent);
+          parentNode = currentParent.parent;
+        } else {
+          break;
+        }
+      }
+
+      result.push(node);
+      result = result.concat(this.getAllSelectedNodes(treeListInstance, childKeys, true));
+    });
+
+    return result;
   }
 }
